15485f161e447b6548c2e50f4f161b6650c76abf
A01564721 <A01564721@tec.mx>
2025-10-16 15:20:55 +0000

    p

---
+++ Parte1.py
@@ -0,0 +1,19 @@
+#Parte 1 
+#Equipo Castores
+#Integrantes: 
+    #Pablo Talamantes 
+    #Oscar Chavez 
+
+palabras = ["El ingeniero estudia Python.", "Python es fundamental en IA."]
+nombre_archivo  = "documento.txt"
+
+ 
+input("Ingrese palabra clave:") 
+
+
+print("¿Desea continuar buscando? (s/n):") 
+ 
+ if
+
+
+
